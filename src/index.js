8b4876d65bcc168b78bb8f0274c6d0033ed08a0d
Samantha Callie <Hoomee90@gmail.com>
2024-01-08 13:39:53 -0800

    add function to get data from httprequest

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // include: Search Tenor. Use this attribution as the placeholder text in your search box.
+
+// Business Logic
+
+function httpGet(url, callback) {
+  let request = new XMLHttpRequest();
+
+  request.addEventListener("loadend", () => {
+    
+    if (request.status === 200) {
+      callback(JSON.parse(request.responseText));
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
+}
