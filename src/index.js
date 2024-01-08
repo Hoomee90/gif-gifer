fff0745db7d53cdb41404276e1ce05f57d9f7874
Samantha Callie <Hoomee90@gmail.com>
2024-01-08 15:05:52 -0800

    make form input search tenor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// include: Search Tenor. Use this attribution as the placeholder text in your search box.
 
 // Business Logic
 
@@ -23,7 +22,7 @@ function httpGet(url, callback) {
 // UI Logic
 
 // callback for the top 8 GIFs of search
-function tenorSearch(responseText) {
+function displayTenorSearch(responseText) {
   const responseObject = JSON.parse(responseText);
   const topGifs = responseObject["results"];
 
@@ -40,15 +39,24 @@ function tenorSearch(responseText) {
   return;
 }
 
-function getData() {
+function getData(searchTerm) {
   const apiKey = process.env.API_KEY;
   const clientKey = "gif-gifer-project";
   const limit = 8;
-  const searchTerm = "pmmm";
 
   const searchUrl = `https://tenor.googleapis.com/v2/search?q=${searchTerm}&key=${apiKey}&client_key=${clientKey}&limit=${limit}`;
 
-  httpGet(searchUrl, tenorSearch);
+  httpGet(searchUrl, displayTenorSearch);
 }
 
-getData();
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const input = document.querySelector("#search-input");
+  getData(input.value);
+  document.querySelector("div.images").classList.remove("d-none");
+
+  e.target.reset();
+}
+
+document.querySelector("form#search").addEventListener("submit", handleSubmit);
