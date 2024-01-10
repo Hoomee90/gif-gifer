83cff55fd46731cd48b6450d0d188f6db715b082
Samantha Callie <Hoomee90@gmail.com>
2024-01-10 15:07:41 -0800

    refactor and move api functions to service object

---
+++ src/tenor.js
@@ -0,0 +1,19 @@
+export default class Tenor {
+  
+  static httpGet(searchTerm) {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      const searchUrl = `https://tenor.googleapis.com/v2/search?q=${searchTerm}&key=${process.env.API_KEY}&client_key=${"gif-gifer-project"}&limit=${8}`;
+      request.addEventListener("loadend", () => {
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve(response);
+        } else {
+          reject(response["error"]["message"]);
+        }
+      });
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
